f46e33d93bc4b46623cdad25627e1d8b7e514588
Nader Alfakesh <nader.alfakesh@gmail.com>
2021-07-13 01:18:38 +0300

    Remove unused screens and components

--- app/components/auto-image/auto-image.story.tsx
+++ app/components/auto-image/auto-image.story.tsx
@@ -6,20 +6,19 @@ import { AutoImage } from "./auto-image"
 
 declare let module
 
-const bowser = require("../../screens/welcome/bowser.png")
 const morty = { uri: "https://rickandmortyapi.com/api/character/avatar/2.jpeg" }
 
 storiesOf("AutoImage", module)
   .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
   .add("Style Presets", () => (
     <Story>
-      <UseCase text="With require()">
+      {/* <UseCase text="With require()">
         <AutoImage source={bowser} />
         <AutoImage source={bowser} style={{ width: 150 }} />
         <AutoImage source={bowser} style={{ width: 150, height: 150 }} />
         <AutoImage source={bowser} style={{ height: 150 }} />
         <AutoImage source={bowser} style={{ height: 150, resizeMode: "contain" }} />
-      </UseCase>
+      </UseCase> */}
       <UseCase text="With URL">
         <AutoImage source={morty} />
         <AutoImage source={morty} style={{ width: 150 }} />
