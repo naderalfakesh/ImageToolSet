ef5429a578cbf9206c3b28c8a044ed71a16946dd
Nader Alfakesh <nader.alfakesh@gmail.com>
2021-07-15 01:49:28 +0300

    Add loading and pressable features to image component

--- app/components/Image/Image.tsx
+++ app/components/Image/Image.tsx
@@ -1,9 +1,23 @@
-import React from "react"
-import { StyleSheet } from "react-native"
+/* eslint-disable react-native/no-color-literals */
+import React, { useState } from "react"
+import { ActivityIndicator, StyleSheet, TouchableOpacity } from "react-native"
 import FastImage, { FastImageProps } from "react-native-fast-image"
 
-const Image = ({ style, ...restProps }: FastImageProps) => {
-  return <FastImage style={[styles.base, style]} {...restProps} />
+const BG_COLOR = "rgba(240, 242, 245, 1)"
+const LOADER_COLOR = "rgba(55, 107, 251, 1)"
+
+interface Props extends FastImageProps {
+  onPress?: () => void
+}
+const Image = ({ style, onPress, ...restProps }: Props) => {
+  const [loaded, setLoaded] = useState(false)
+
+  return (
+    <TouchableOpacity style={styles.base} onPress={onPress} disabled={!onPress}>
+      <FastImage style={[styles.base, style]} onLoad={() => setLoaded(true)} {...restProps} />
+      {!loaded && <ActivityIndicator color={LOADER_COLOR} style={styles.loader} />}
+    </TouchableOpacity>
+  )
 }
 
 export default Image
@@ -12,5 +26,9 @@ const styles = StyleSheet.create({
   base: {
     height: "100%",
     width: "100%",
+  },
+  loader: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: BG_COLOR,
   },
 })
