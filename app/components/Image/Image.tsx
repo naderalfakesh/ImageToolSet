64585abfd763d615b07bd9d0595cb7a12bde58e9
Nader Alfakesh <nader.alfakesh@gmail.com>
2021-07-15 16:08:29 +0300

    Add Image input field

--- app/components/Image/Image.tsx
+++ app/components/Image/Image.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-color-literals */
 import React, { useState } from "react"
-import { ActivityIndicator, StyleSheet, TouchableOpacity } from "react-native"
+import { ActivityIndicator, StyleSheet, TouchableOpacity, ViewStyle } from "react-native"
 import FastImage, { FastImageProps } from "react-native-fast-image"
 
 const BG_COLOR = "rgba(240, 242, 245, 1)"
@@ -8,12 +8,13 @@ const LOADER_COLOR = "rgba(55, 107, 251, 1)"
 
 interface Props extends FastImageProps {
   onPress?: () => void
+  containerStyle?: ViewStyle
 }
-const Image = ({ style, onPress, ...restProps }: Props) => {
+const Image = ({ style, containerStyle, onPress, ...restProps }: Props) => {
   const [loaded, setLoaded] = useState(false)
 
   return (
-    <TouchableOpacity style={styles.base} onPress={onPress} disabled={!onPress}>
+    <TouchableOpacity style={[styles.base, containerStyle]} onPress={onPress} disabled={!onPress}>
       <FastImage style={[styles.base, style]} onLoad={() => setLoaded(true)} {...restProps} />
       {!loaded && <ActivityIndicator color={LOADER_COLOR} style={styles.loader} />}
     </TouchableOpacity>
