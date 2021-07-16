eaebd02791984fa3c7745a1f469975fde41c9d2c
Nader Alfakesh <nader.alfakesh@gmail.com>
2021-07-16 19:37:31 +0300

    Add fruits list component

---
+++ app/screens/Main/FruitsList.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import { FlatList, View, StyleSheet } from "react-native"
+import ImageWithCaption from "../../components/ImageWithCaption"
+import { Photo } from "./types"
+
+interface Props {
+  images: Photo[]
+  onImagePress?: (id) => void
+}
+
+const FruitsList = ({ images, onImagePress }: Props) => {
+  return (
+    <View>
+      <FlatList<Photo>
+        contentContainerStyle={styles.container}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        data={images}
+        renderItem={({ item, index }) => (
+          <ImageWithCaption
+            style={index && styles.item}
+            key={item.id}
+            url={item.url}
+            caption={item.title}
+            onPress={() => onImagePress(item.id)}
+          />
+        )}
+      />
+    </View>
+  )
+}
+
+export default FruitsList
+
+const styles = StyleSheet.create({
+  container: { paddingHorizontal: 24, paddingVertical: 20 },
+  item: { marginLeft: 12 },
+})
